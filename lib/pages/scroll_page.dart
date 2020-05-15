015bd62a7be2bfd8d3c476e4d7f26c56004c3658
mari <mari.17ymy@gmail.com>
2020-05-14 23:21:01 -0400

    imagen y texto scroll

--- lib/pages/scroll_page.dart
+++ lib/pages/scroll_page.dart
@@ -4,8 +4,8 @@ class ScrollPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: PageView(
+    return Scaffold(
+      body: PageView(
         scrollDirection: Axis.vertical,
         children: <Widget>[
           _pagina1(),
@@ -16,9 +16,42 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1() {
-    return Text('HOLA');
+    return Stack(
+      children: <Widget>[
+        _img(),
+        _textos()
+      ],
+    );
   }
+  Widget _img() {
+    return Image(
+      image: AssetImage('assets/scroll-1.png'),
+      fit: BoxFit.cover,
+      width: double.infinity,
+      height: double.infinity,
+    );
+  }
+  Widget _textos() {
+    TextStyle estiloTexto = TextStyle(color: Colors.white, fontSize: 40.0);
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0,),
+          Text('11°', style: estiloTexto, ),
+          Text('Miercoles', style: estiloTexto,),
+          Expanded(child: Container(),),
+          Icon(Icons.keyboard_arrow_down, color: Colors.white, size: 60.0,),
+        ],
+      ),
+    );
+        
+  }
+
+
+
   Widget _pagina2() {
-    return Text(':***');
+    return Center(child: Text(':***'));
   }
+
+  
 }
